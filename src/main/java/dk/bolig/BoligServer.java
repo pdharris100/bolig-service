f07d260c9fd2211ded837b7a639e83b1d501b80e
Paul <pdharris@gmail.com>
2019-02-04 17:56:55 +0100

    Prepare for tomcat deployment

--- src/main/java/dk/bolig/BoligServer.java
+++ src/main/java/dk/bolig/BoligServer.java
@@ -5,11 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import dk.bolig.controller.EstimateController;
 
 @SpringBootApplication
-public class BoligServer {
+public class BoligServer extends SpringBootServletInitializer{
 	private static final Logger LOG = LoggerFactory.getLogger(EstimateController.class);
     public static void main(String[] args) {
         SpringApplication.run(BoligServer.class, args);
