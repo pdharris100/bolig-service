20003d07dcc14133d5aecb0ff02c1ca54a38b2a2
Paul <pdharris@gmail.com>
2019-04-05 17:32:24 +0200

    CORS support

--- src/main/java/dk/bolig/controller/EstimateController.java
+++ src/main/java/dk/bolig/controller/EstimateController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,14 @@ import dk.bolig.dto.EstimateDTO;
 import dk.bolig.service.SalesHistoryService;
 import dk.bolig.tools.PriceEstimator;
 
+@CrossOrigin
 @RestController
 public class EstimateController {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(EstimateController.class);
 
-    @GetMapping("/estimate")
+
+	@GetMapping("/estimate")
     public EstimateDTO estimate(@RequestParam(value="postcode") String postcode,
     		@RequestParam(value="street") String street) throws IOException {
     	LOG.debug("********** Estimating for " + street + " " + postcode);
