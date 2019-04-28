273cadeaf18f03ef53ea2db7176cdea1ce9fc2e6
Paul <pdharris@gmail.com>
2019-04-28 21:13:50 +0200

    Ugly outlier removal hack

--- src/main/java/dk/bolig/service/SalesHistoryService.java
+++ src/main/java/dk/bolig/service/SalesHistoryService.java
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class SalesHistoryService {
 	private static final Logger LOG = LoggerFactory.getLogger(SalesHistoryService.class);
-    
+
 	public static final String BASE_URL = "https://www.boliga.dk/salg/resultater?so=1&sort=omregnings_dato-a&maxsaledate=today";
 	public static final String POST_CODE_URL = "iPostnr";
 	public static final String STREET_URL = "gade";
@@ -29,95 +29,102 @@ public class SalesHistoryService {
 	public static final String ORIGINAL_STREET_URL = "origgade";
 	public static final String AND = "&";
 	public static final String EQUALS_URL = "=";
-	
-    //@Cacheable("estimate")
+
+	// @Cacheable("estimate")
 	public double[][] getSalesDataForPostCodeAndStreet(String postCode, String street) throws IOException {
 		postCode = processPostCode(postCode);
 		String type = "Ejerlejlighed";
-		
-        String url = BASE_URL +
-				AND + POST_CODE_URL + EQUALS_URL + postCode +
-				AND + STREET_URL + EQUALS_URL + street +
-				AND + TYPE_URL + EQUALS_URL + type +
-				AND + "minsaledate=2009&origgade=" + street;
-		
-        int page = 1; 
-        List <double []> dataSet = new ArrayList <double []>();
+
+		String url = BASE_URL + AND + POST_CODE_URL + EQUALS_URL + postCode + AND + STREET_URL + EQUALS_URL + street
+				+ AND + TYPE_URL + EQUALS_URL + type + AND + "minsaledate=2009&origgade=" + street;
+
+		int page = 1;
+		List<double[]> dataSet = new ArrayList<double[]>();
 		while (true) {
 			Document doc = Jsoup.connect(url + "&p=" + page).get();
 			if (rowsExist(doc)) {
 				process(doc, dataSet);
-				page ++;
+				page++;
 			} else {
 				break;
-			}			
-		}  
+			}
+		}
 
-		double [][] dataArray = new double [dataSet.size()][]; 
+		double[][] dataArray = new double[dataSet.size()][];
 		dataArray = dataSet.toArray(dataArray);
-	    
+
 		return dataArray;
-    }
-	
-	private void process(Document doc, List <double []> dataSet) {
-        Elements rows = doc.select("[class=d-md-none d-block]");
-        for (Element row : rows) {
-        	String dateElement = row.select("h5:eq(1)").toString();
-            String dateString = parseDate(dateElement); 
-			if ("error".equals(dateString)) continue;
-			
-            Date date;
+	}
+
+	private void process(Document doc, List<double[]> dataSet) {
+		Elements rows = doc.select("[class=d-md-none d-block]");
+		for (Element row : rows) {
+			String dateElement = row.select("h5:eq(1)").toString();
+			String dateString = parseDate(dateElement);
+			if ("error".equals(dateString))
+				continue;
+
+			Date date;
 			try {
 				date = new SimpleDateFormat("dd-MM-yyyy").parse(dateString);
 			} catch (ParseException e) {
 				continue;
 			}
-			
-            String price = parsePrice(row.select("h5:eq(3)").toString());             
-            if (price.equals("error")) continue;
-            
-            LOG.debug(date + " " + price);
-            
-            dataSet.add(new double [] { Double.valueOf(date.getTime()), Double.valueOf(price) });
-        }
+
+			String price = parsePrice(row.select("h5:eq(3)").toString());
+			if (price.equals("error"))
+				continue;
+
+			LOG.debug(date + " " + price);
+
+			dataSet.add(new double[] { Double.valueOf(date.getTime()), Double.valueOf(price) });
+		}
 	}
-	
-    private String processPostCode(String postCode) {
-    	int postCodeAsInt = Integer.valueOf(postCode).intValue();
-    	if ( postCodeAsInt >= 1050 && postCodeAsInt <= 1549 ) {
-    		return "1050-1549";
-    	} else if (postCodeAsInt >= 1550 && postCodeAsInt <= 1799 ) {
-    		return "1550-1799";
-    	} else if (postCodeAsInt >= 1800 && postCodeAsInt <= 1999 ) {
-    		return "1800-1999";
-    	} else {
-    		return postCode;    		
-    	}		
+
+	private String processPostCode(String postCode) {
+		int postCodeAsInt = Integer.valueOf(postCode).intValue();
+		if (postCodeAsInt >= 1050 && postCodeAsInt <= 1549) {
+			return "1050-1549";
+		} else if (postCodeAsInt >= 1550 && postCodeAsInt <= 1799) {
+			return "1550-1799";
+		} else if (postCodeAsInt >= 1800 && postCodeAsInt <= 1999) {
+			return "1800-1999";
+		} else {
+			return postCode;
+		}
 	}
 
 	private boolean rowsExist(Document doc) {
-    	return !(doc.select("[class=d-md-none d-block]").isEmpty());
+		return !(doc.select("[class=d-md-none d-block]").isEmpty());
 	}
-		
+
 	private String parsePrice(String string) {
-    	String [] tokens = string.split(" k");
+		String[] tokens = string.split(" k");
 		String price = tokens[0].substring(17);
-        NumberFormat nf = NumberFormat.getInstance(Locale.GERMAN);	
- 		try {
-			price = nf.parse(price).toString();
+
+		NumberFormat nf = NumberFormat.getInstance(Locale.GERMAN);
+
+		try {
+			Number priceNum = nf.parse(price);
+			if (priceNum.longValue() > 150000) {
+				price = "error";
+			} else {
+				price = priceNum.toString();
+			}
 		} catch (Exception e) {
 			price = "error";
 		}
+
 		return price;
-		
+
 	}
-	
+
 	private String parseDate(String string) {
-    	if (!string.contains("Alm.")) {
-    		return "error";
-    	}
-		String [] tokens = string.split(",");
+		if (!string.contains("Alm.")) {
+			return "error";
+		}
+		String[] tokens = string.split(",");
 		String date = tokens[0].substring(4);
-		return date;		
+		return date;
 	}
 }
